81a2e4818d448d3459dadd539e9c0619834cf9bd
Mobey-eth <chantler.aob@gmail.com>
2023-09-23 11:39:14 +0100

    EJS commit

--- app.js
+++ app.js
@@ -3,26 +3,48 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+// app.set("views", "mobiViews"); // to set custom views path
+
 // listen for requests
 
 app.listen(3000);
 
 app.get("/", (req, res) => {
   // res.send("<p>Home Page</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+
+  const blogs = [
+    {
+      title: "Moby is a Whale",
+      snippet: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    },
+    {
+      title: "Moby Loves plantains",
+      snippet: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    },
+    {
+      title: "Moby OTR?",
+      snippet: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    },
+  ];
+
+  res.render("index", { title: "Home Page", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  // res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
 // redirects
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create" });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
-}); // Used to create & fire middleware fxns in express
+  res.status(404).render("404", { title: "404" });
+  //  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
